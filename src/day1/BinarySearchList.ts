4f8a1fb369ccb5b72576a0b34b69e89f26f10e90
Sanjero20 <gelosantos72@gmail.com>
2024-06-28 15:21:06 +0800

    Add solution for binary search list

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,21 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
 
+    do {
+        // Find the midpoint by dividing the low and high by half
+        const mid = Math.floor(lo + (hi - lo) / 2);
+        const val = haystack[mid];
+
+        if (val === needle) {
+            return true;
+        } else if (val < needle) {
+            lo = mid + 1;
+        } else {
+            hi = mid;
+        }
+    } while (lo < hi);
+
+    // Does not exist in the array
+    return false;
 }
