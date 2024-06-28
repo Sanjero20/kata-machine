9594a64a3c470f02c9c4c4b46e70557b17379ecb
Sanjero20 <gelosantos72@gmail.com>
2024-06-28 15:36:56 +0800

    Add solution for LinearSearch List

--- src/day1/LinearSearchList.ts
+++ src/day1/LinearSearchList.ts
@@ -1,3 +1,11 @@
-export default function linear_search(haystack: number[], needle: number): boolean {
+export default function linear_search(
+    haystack: number[],
+    needle: number,
+): boolean {
+    for (let i = 0; i < haystack.length; i++) {
+        const val = haystack[i];
+        if (val === needle) return true;
+    }
 
+    return false;
 }
