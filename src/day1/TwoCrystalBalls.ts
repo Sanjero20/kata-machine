01af3bc4edf415ef047c3240034cf99e52e2f2f9
Sanjero20 <gelosantos72@gmail.com>
2024-06-28 15:34:58 +0800

    Add solution for TwoCrystalBalls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,25 @@
-export default function two_crystal_balls(breaks: boolean[]): number {
+// Given two crystal balls that will break if dropped from high enough
+// distance, determine the exact spot in which it will break in the most
+// optimized way.
 
+export default function two_crystal_balls(breaks: boolean[]): number {
+    const steps = Math.floor(Math.sqrt(breaks.length));
+    let prev = 0;
+
+    for (let i = 0; i <= breaks.length; i += steps) {
+        if (breaks[i] == true) {
+            break;
+        }
+
+        prev = i;
+    }
+
+    // Linear search
+    for (let j = prev; j <= breaks.length; j++) {
+        if (breaks[j] == true) {
+            return j;
+        }
+    }
+
+    return -1;
 }
