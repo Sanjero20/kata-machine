2c16e000d99c990e8b9745cd462e8a5d630104c3
Sanjero20 <gelosantos72@gmail.com>
2024-07-01 23:24:33 +0800

    Add solution for BubbleSort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,12 @@
 export default function bubble_sort(arr: number[]): void {
-
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            // Swap if the current index is bigger than the next index
+            if (arr[j] > arr[j + 1]) {
+                const tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
 }
